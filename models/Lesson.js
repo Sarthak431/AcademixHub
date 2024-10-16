ef6ed9e05d15bc1101cd3df894981ef44309466b
Sarthak431 <sssarthak431@gmail.com>
2024-10-16 22:51:32 +0530

    Resolved merge conflicts 4

--- models/Lesson.js
+++ models/Lesson.js
@@ -1,34 +1,5 @@
 import mongoose from 'mongoose';
 
-<<<<<<< HEAD
-const lessonSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, "A lesson must have a title"],
-  },
-  content: {
-    type: String,
-    required: [true, "A lesson must have content"],
-  },
-  duration: {
-    type: Number,
-    required: [true, "A lesson must have a duration"],
-    min: [1, "Duration must be at least 1 minute"],
-  },
-  course: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Course",
-    required: [true, "A lesson must belong to a course"],
-  }
-}, {
-  timestamps: true 
-});
-
-const Lesson = mongoose.model("Lesson", lessonSchema);
-
-export default Lesson;
-
-=======
 const lessonSchema = new mongoose.Schema({
   title: {
     type: String,
@@ -60,8 +31,3 @@ const Lesson = mongoose.model('Lesson', lessonSchema);
 
 export default Lesson;
 
-<<<<<<< HEAD
-
-=======
->>>>>>> c9a0c724aff241a01a61cae071ef17014cd335ea
->>>>>>> 691d433aadfb7aa620a8adf345bc4a2645184daa
--- models/Review.js
+++ models/Review.js
@@ -1,91 +1,8 @@
 import mongoose from "mongoose";
 import Course from "./Course.js";
 
-<<<<<<< HEAD
-const reviewSchema = new mongoose.Schema(
-  {
-    review: {
-      type: String,
-      required: [true, "Review cannot be empty"],
-    },
-    rating: {
-      type: Number,
-      min: [1, "Rating must be above 1.0"],
-      max: [5, "Rating must be below 5.0"],
-      required: [true, "A rating is required"],
-    },
-    course: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Course",
-      required: [true, "Review must belong to a course"],
-    },
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: [true, "Review must belong to a user"],
-    },
-  },
-  {
-    timestamps: true,
-  },
-  {
-    toJSON: { virtuals: true },
-    toObject: { virtuals: true },
-  }
-);
 
-const calculateAverageRating = async function (courseId) {
-  const reviews = await mongoose.model("Review").find({ course: courseId });
-  const totalReviews = reviews.length;
 
-  if (totalReviews > 0) {
-    const totalRating = reviews.reduce((acc, review) => acc + review.rating, 0);
-    const averageRating = totalRating / totalReviews;
-
-    await Course.findByIdAndUpdate(courseId, {
-      rating: Math.round(averageRating * 10) / 10,
-    });
-  } else {
-    await Course.findByIdAndUpdate(courseId, { rating: 0 });
-  }
-};
-
-reviewSchema.post("save", async function (doc) {
-  await calculateAverageRating(doc.course);
-});
-
-reviewSchema.post("findOneAndUpdate", async function (doc) {
-  if (doc) {
-    await calculateAverageRating(doc.course);
-  }
-});
-
-reviewSchema.pre("findOneAndDelete", async function (next) {
-  const review = await this.model.findOne(this.getQuery());
-
-  if (review) {
-    const course = await Course.findById(review.course);
-    if (course) {
-      const Review = mongoose.model("Review");
-
-      const result = await Review.aggregate([
-        { $match: { course: course._id, _id: { $ne: review._id } } },
-        { $group: { _id: "$course", averageRating: { $avg: "$rating" } } },
-      ]);
-
-      course.rating = result.length > 0 ? Math.round(result[0].averageRating * 10) / 10 : 0;
-
-      await course.save();
-    }
-  }
-  next();
-});
-
-const Review = mongoose.model("Review", reviewSchema);
-
-export default Review;
-
-=======
 const reviewSchema = new mongoose.Schema(
   {
     review: {
@@ -171,8 +88,3 @@ const Review = mongoose.model("Review", reviewSchema);
 
 export default Review;
 
-<<<<<<< HEAD
-
-=======
->>>>>>> c9a0c724aff241a01a61cae071ef17014cd335ea
->>>>>>> 691d433aadfb7aa620a8adf345bc4a2645184daa
